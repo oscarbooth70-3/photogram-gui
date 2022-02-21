5ab202ad7d2f4b8508bd410032a21c658720af0a
odavilau <odavilau@chicagobooth.edu>
2022-02-21 01:03:49 +0000

    OscarProgress7

--- app/controllers/photos_controller.rb
+++ app/controllers/photos_controller.rb
@@ -3,14 +3,14 @@ class PhotosController < ApplicationController
   def index
     matching_photos = Photo.all
     
-    @list_of_photos = matching_photos.order({ :id => :asc})
+    @list_of_photos = matching_photos.order({ :created_at => :desc })
     
     render({ :template => "photo_templates/index.html.erb"})
 
   end
 
   def show
-    url_photo_id = params.fetch("path_photo")
+    url_photo_id = params.fetch("path_id")
     
     matching_photos = Photo.where({ :id => url_photo_id })
     
